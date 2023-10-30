5ac0104ffb37bed2f2af1597fcd7f38e92ba57f5
Milan Unger <nilan.unger@siemens-healthineers.com>
2023-10-30 18:14:04 +0100

    Add context attributes and data props

--- web-ui/src/components/ufe-context/ufe-context.tsx
+++ web-ui/src/components/ufe-context/ufe-context.tsx
@@ -8,9 +8,17 @@ import { getUfeRegistryAsync, UfeRegistry } from 'ufe-registry';
 })
 export class UfeContext {
 
+  // name of the context to render web components from contex configurations
   @Prop() context: string 
 
+  // selector to match the elements to render
   @Prop() selector: string ;
+
+  // attributes will be propagated to rendered web components
+  @Prop() attributes: { [key: string]: string}
+
+  // if set, then the "data" property will be assigned to the rendered components
+  @Prop() data: any;
 
   private ufeRegistry: UfeRegistry;
 
@@ -18,9 +26,7 @@ export class UfeContext {
     this.ufeRegistry = await getUfeRegistryAsync();
   }
 
-  
-
-  
+ 
   render() {
     const contextElements = this.ufeRegistry.contextElements( this.context, this.selector );
     this.ufeRegistry.preloadDependenciesAsync(contextElements);
@@ -30,6 +36,7 @@ export class UfeContext {
       <Host>
         <slot name="beforeALl"></slot>
         {contextElements.map( el => { 
+          el.attributes = Object.assign({}, el.attributes, this.attributes);
           return (
             <context-item>
               <slot name="before-each"></slot>
